bc2c347671e39665ee466784dcc162293a55f41d
yogesh <codes.yogesh67@gmail.com>
2025-10-03 12:35:27 -0400

    adding featuresection in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 // import QuizPage from "./quiz/page";
 import HeroLanding from "@/components/layout/hero-landing";
 // import CategoryGrid from "@/components/layout/category-grid";
-// import FeatureStrip from "@/components/layout/feature-strip";
+import FeatureStrip from "@/components/layout/feature-strip";
 // import PricingTeaser from "@/components/layout/pricing-teaser";
 
 export default function Home() {
   return (
     <>
       <HeroLanding />
-      {/* <FeatureStrip />
-      <CategoryGrid /> */}
+      <FeatureStrip />
+      {/* <CategoryGrid /> */}
 
       {/* <div className="px-2 sm:px-4 md:px-10 lg:px-20 xl:px-28 max-w-7xl mx-auto py-10">
         <h2 className="text-center text-6xl font-bold mb-2">
