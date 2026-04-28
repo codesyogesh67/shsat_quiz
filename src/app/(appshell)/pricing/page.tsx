5881150f54d8eb223b102ab1992f9cf80e9248e5
yogesh <codes.yogesh67@gmail.com>
2026-04-28 07:06:45 -0400

    Fix pricing page access type mismatch

--- src/app/(appshell)/pricing/page.tsx
+++ src/app/(appshell)/pricing/page.tsx
@@ -29,6 +29,7 @@ export default async function PricingPage() {
         trialEndsAt: true,
         premiumStartedAt: true,
         premiumEndsAt: true,
+        subscriptionStatus: true,
       },
     });
 
